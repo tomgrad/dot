84da5b2d32c36381e4dc3f0a3a3ffa83f15d199f
Tomasz Gradowski <tomgrad@if.pw.edu.pl>
2016-11-10 11:54:29 +0100

    nvim

--- linux-backup.sh
+++ linux-backup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 cp ~/.zshrc ./zshrc-linux
-cp ~/.vimrc ./vimrc-linux
+#cp ~/.vimrc ./vimrc-linux
+cp ~/.config/nvim/init.vim ./nvim
+
+cp ~/.config/nvim/autoload/* ./nvim/autoload
 cp ~/.config/Code/User/snippets/* ./vscode-snippets
 
