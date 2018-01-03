87cc21cce772359e2a55ba68984d02854def6ee7
Tomasz Gradowski <tomgrad@gmail.com>
2018-01-03 22:53:35 +0100

    copy dunst

--- linux-backup.sh
+++ linux-backup.sh
@@ -7,6 +7,8 @@ cp ~/.config/i3/config ./.config/i3
 cp ~/.config/i3status/config ./.config/i3status
 cp ~/.config/compton.conf ./.config
 cp -r ~/.config/ranger ./.config
+cp -r ~/.config/dunst ./.config
+
 cp ~/.vimrc .
 cp ~/.xprofile .
 #cp ~/.config/nvim/init.vim ./nvim
